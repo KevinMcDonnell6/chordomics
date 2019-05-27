3ab054a2ef8a78e44b67db6f49d949383dc0fdc6
Nick Waters <nickp60@gmail.com>
2019-05-27 18:38:00 +0100

    refactor COG_names.R; add tests

--- R/COG_names.R
+++ R/COG_names.R
@@ -1,41 +1,48 @@
-
-
 COG_names <- function(df,UniqueCOGs = "UniqueCOGs"){
-  
+  # see https://stackoverflow.com/questions/9521009/
+  # loads the categories object into env; otherwise, tests wont work
+  data(categories, envir=environment())
+  data(funGroups, envir=environment())
   df <- as.data.frame(df)
-  
-  len <- nrow(df)
-  df$COG_Name <- ""
-  df$GroupLetters <- ""
-  df$COG_Categorys <- ""
-  
-  for(i in 1:len){
-    # print(df$Group)
-    
-    if(!is.null(df[i,UniqueCOGs]) & !is.na(df[i,UniqueCOGs]) & as.character(df[i,UniqueCOGs]) != "" & any(stringr::str_detect(categories$COG,as.character(df[i,UniqueCOGs])))){
-      # print(i)
-      res <- categories[stringr::str_detect(categories$COG,as.character(df[,UniqueCOGs][i])),]
-      df$COG_Name[i] <- res$name
-      df$GroupLetters[i] <- res$func
-      # print(df$GroupLetter[i])
-      GL <- unlist(strsplit(df$GroupLetters[i],""))
-      for(letter in GL){
-        
-        res <- funGroups[stringr::str_detect(funGroups$Code,letter),]
-        # print(res$Name)
-        df$COG_Categorys[i] <- ifelse(df$COG_Categorys[i]=="",
-                                       res$Name,
-                                       paste(df$COG_Categorys[i],res$Name,sep = ";")
-                                       )
-        } # close loop through letters
-      
-      } # close if statement
-    
-    } # close for loop through reads
-  
 
-  df$COG_Category <- df$COG_Categorys
-  
-  
- return(df) 
+  # ensure that only one listed per row
+  if (any(grepl(" ",df$UniqueCOGs ))){
+    warning("No more than a single COG should be provided per row! If multiple CDS on a sequence, split into multiple rows. For now, we will select the first... ")
+    # select the first one only
+    for(i in 1: nrow(df)){
+      df[i, UniqueCOGs] <- strsplit(df[i, UniqueCOGs], " ")[[1]][1]
+    }
+  }
+
+  df <- dplyr::left_join(df, categories, by=c(UniqueCOGs = "COG"))
+  df <- dplyr::rename(df, COG_Category = "func", COG_Name = "name")
+  # for(i in 1:len){
+  #   # print(df$Group)
+  #
+  #   if(!is.null(df[i,UniqueCOGs]) & !is.na(df[i,UniqueCOGs]) & as.character(df[i,UniqueCOGs]) != "" & any(stringr::str_detect(categories$COG,as.character(df[i,UniqueCOGs])))){
+  #     # print(i)
+  #     res <- categories[stringr::str_detect(categories$COG,as.character(df[,UniqueCOGs][i])),]
+  #     df$COG_Name[i] <- res$name
+  #     df$GroupLetters[i] <- res$func
+  #     # print(df$GroupLetter[i])
+  #     GL <- unlist(strsplit(df$GroupLetters[i],""))
+  #     for(letter in GL){
+  #
+  #       res <- funGroups[stringr::str_detect(funGroups$Code,letter),]
+  #       # print(res$Name)
+  #       df$COG_Categorys[i] <- ifelse(df$COG_Categorys[i]=="",
+  #                                      res$Name,
+  #                                      paste(df$COG_Categorys[i],res$Name,sep = ";")
+  #                                      )
+  #       } # close loop through letters
+  #
+  #     } # close if statement
+  #
+  #   } # close for loop through reads
+  #
+  #
+  # df$COG_Category <- df$COG_Categorys
+
+
+ return(df)
 }
--- tests/testthat/test_COG_names.R
+++ tests/testthat/test_COG_names.R
@@ -1,15 +1,29 @@
 context("LCA_alg")
 library(chordomics)
 
-test_data <- data.frame(uniprots=c("P21514, P21515", "P41543"), other=c("other", "data"), stringsAsFactors = F)
-test_results <- cbind(test_data, data.frame(Uniprot=c("P21514, P21515", "P41543"),
-                                            AllCOGs=c("COG2200 COG2771 COG3124", ""),
-                                            UniqueCOGs=c("COG2200 COG2771 COG3124", ""), stringsAsFactors = F)
-                      )
+test_data <- data.frame(
+  uniprots = c("P21514, P21515", "P41543"),
+  other = c("other", "data"),
+  Uniprot = c("P21514, P21515", "P41543"),
+  AllCOGs = c("COG2200 COG2771 COG3124", ""),
+  UniqueCOGs = c("COG2200 COG2771 COG3124", ""),
+  stringsAsFactors = F
+)
+results <- structure(
+  list(
+    uniprots = c("P21514, P21515", "P41543"),
+    other = c("other", "data"),
+    Uniprot = c("P21514, P21515", "P41543"),
+    AllCOGs = c("COG2200 COG2771 COG3124",  ""),
+    UniqueCOGs = c("COG2200", NA),
+    COG_Category = c("T", NA),
+    COG_Name = c("EAL domain, c-di-GMP-specific phosphodiesterase class I (or its enzymatically inactive variant)", NA)),
+  row.names = c(NA, -2L),
+  class = "data.frame")
 test_that("Check function fetching list of COGs for uniprot IDs", {
   expect_equal(
-    chordomics:::Get_COG(df=test_data, UniprotColumn="uniprots"),
-    test_results
+    chordomics:::COG_names(df=test_data, UniqueCOGs = "UniqueCOGs"),
+    results
   )
 })
 
