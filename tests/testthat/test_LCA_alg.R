94baeef72443e9eaeb77e6cd0268bfe76633b428
Nick Waters <nickp60@gmail.com>
2019-05-29 12:47:45 +0100

    fix tests

--- tests/testthat/test_LCA_alg.R
+++ tests/testthat/test_LCA_alg.R
@@ -1,11 +1,26 @@
 context("LCA_alg")
 library(chordomics)
+#processData <-  processMGRAST("mgm4762935.3", DATA_DIR, output, e=environment())
+#dput(processData[1:6,1:3])
+test_input <-
+  structure(
+    list(
+      id = c(
+        "mgm4762935.3|contig_101_1169455_length_3868_multi_2_in_0_out_0_1_1043_+",
+        "mgm4762935.3|contig_101_1488739_length_29947_multi_2_in_0_out_0_1_611_-",
+        "mgm4762935.3|contig_101_1488739_length_29947_multi_2_in_0_out_0_15169_16711_+",
+        "mgm4762935.3|contig_101_1488739_length_29947_multi_2_in_0_out_0_21654_22613_-",
+        "mgm4762935.3|contig_101_1488739_length_29947_multi_2_in_0_out_0_24170_25363_-",
+        "mgm4762935.3|contig_101_1488739_length_29947_multi_2_in_0_out_0_27029_27858_+"
+      ),
+      taxids_by_seq = c("55802","83483", "53363;1744;69014;49338","61435","2208;2214;33075","2209;49338"),
+      COGs_by_seq = c(NA, "COG0013", "COG0513", "COG2041", "COG0229", "COG0605")),
+    row.names = c(NA, 6L),
+    class = "data.frame"
+  )
 
-test_data <- read.csv("~/Downloads/day1R2_curated_Head.csv")
-test_that("str_length is number of characters", {
-  #expect_equal(LCA_alg(test_data), 1)
-  #expect_equal(LCA_alg(test_data), 2)
-  #expect_equal(LCA_alg(test_data), 3)
-  expect_equal(3,3)
+test_that("Test assign_taxa", {
+  expect_equal(c("Euryarchaeota", "Euryarchaeota", "", "Chloroflexi", "", ""),
+               chordomics:::assign_taxa(df = test_input)$phylum)
 })
 
