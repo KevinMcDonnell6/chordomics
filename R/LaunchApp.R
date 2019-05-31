1ec62e308ad12017e6c9f7ae6c6639732cc87475
Nick Waters <nickp60@gmail.com>
2019-05-31 09:41:36 +0100

    [WIP] try modifying onStart for resource loading

--- R/LaunchApp.R
+++ R/LaunchApp.R
@@ -1,11 +1,16 @@
 #' Launch Chordomics
-#' 
+#'
 #' Function launches Chordomics server and UI
 #' @export
-#' 
+#'
 
 launchApp <- function(){
   shinyjs::useShinyjs()
-  shiny::shinyApp(server = ChordShinyAppServer, ui = ChordShinyAppUI, options = list(launch.browser=T))
+  shiny::shinyApp(server = ChordShinyAppServer,
+                  ui = ChordShinyAppUI,
+                  onStart =  function() {
+                     shiny::addResourcePath("svg-crowbar.js", system.file("www", "svg-crowbar.js", package="chordomics"))
+                  },
+                  options = list(launch.browser=T))
 }
 
